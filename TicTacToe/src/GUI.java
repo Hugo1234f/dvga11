66b8a92aaee44ef7e764b01fa8087fa26af2faaa
Hugo Friberg <friberghugo@gmail.com>
2022-04-02 19:19:31 +0200

    alpha

--- TicTacToe/src/GUI.java
+++ TicTacToe/src/GUI.java
@@ -4,8 +4,6 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.Image;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 
@@ -16,54 +14,50 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class GUI extends JFrame implements ActionListener {
-	private static final long serialVersionUID = 1085496835987043717L;
-	JPanel topBar, gameArea, bottomBar;
-	private JButton[] btns;
+public class GUI extends JFrame{
 	
-	public GUI() {
-		//Set up the window
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(600,500);
-		setResizable(false);
-		setPreferredSize(new Dimension(500,600));
-		setMinimumSize(new Dimension(400,500));
-		setVisible(true);
-		setLayout(new BorderLayout());
-		
-		//Initialize the JPanels
-		topBar = new JPanel(new FlowLayout(FlowLayout.CENTER, 50, 10));
-		gameArea = new JPanel(new GridLayout(3,3));
-		bottomBar = new JPanel(new FlowLayout());
+	private JPanel top, bottom, game;
+	private JButton[] btns;
+	private final Color player1Color = Color.red;
+	private final Color player2Color = Color.blue;
+	private final Color defaultColor;
+	
+	public GUI(Color defaulColor) {
 		btns = new JButton[9];
+		this.defaultColor = defaulColor;
 		
-		//Adds content to the panels
-		topBar.add(new JLabel("Player O Score"), FlowLayout.LEFT);
-		topBar.add(new JLabel("It's O's turn"), FlowLayout.CENTER);
-		topBar.add(new JLabel("Player X Score"), FlowLayout.RIGHT);
+		//Set up window
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+		setPreferredSize(new Dimension(1280, 720));
+	
+		//Set up layouts
+		setLayout(new BorderLayout());
+		top = new JPanel(new FlowLayout());
+		game = new JPanel(new GridLayout(3,3));
+		bottom = new JPanel(new FlowLayout());
 		
+		//Add content to the layouts
+		top.add(new JLabel("Test"));
 		for(int i = 0; i < 9; i++) {
 			btns[i] = new JButton(Integer.toString(i));
 			
-			btns[i].setBackground(Color.white);
+			btns[i].setBackground(defaultColor);
 			btns[i].setRolloverEnabled(false);
-			btns[i].addActionListener(this);
 			
-			gameArea.add(btns[i]);
+			game.add(btns[i]);
 		}
+		bottom.add(getLogo());
 		
-		bottomBar.add(getLogo());
 		
-		
-		//Adds the JPanels and packs the frame
-		this.add(topBar, BorderLayout.NORTH);
-		this.add(gameArea, BorderLayout.CENTER);
-		this.add(bottomBar, BorderLayout.SOUTH);
-		this.pack();
+		//Add the layouts to the frame
+		add(top, BorderLayout.NORTH);
+		add(game, BorderLayout.CENTER);
+		add(bottom, BorderLayout.SOUTH);
+		pack();
 	}
 	
-	//ImageIO och ImageIcon hämtat från:
-	//	https://stackoverflow.com/questions/299495/how-to-add-an-image-to-a-jpanel
+	//tries to read tttlogo.png and either returns it, or a error message as a JLabel
 	//getScaledInstance hämtat från:
 	//	https://stackoverflow.com/questions/5895829/resizing-image-in-java
 	private JLabel getLogo() {
@@ -76,18 +70,24 @@ public class GUI extends JFrame implements ActionListener {
 		
 		scaledLogo = logo.getScaledInstance(60, 40, Image.SCALE_DEFAULT);
 		return new JLabel(new ImageIcon(scaledLogo));
-		
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		for(int i = 0; i < btns.length; i++) {
-			if(e.getSource() == btns[i]) {
-				System.out.println("Button " + (i+1) + " was pressed");
-			}
-		}
 	}
 	
+	//Returns the JButton at the requested index if it exists. Otherwise throws an exception
+	public JButton getButton(int index) throws IndexOutOfBoundsException {
+		return btns[index];
+	}
+	
+	//sets the JButton at the requested index if it exists. Otherwise throws an exception
+	public void setButton(int index, JButton value) throws IndexOutOfBoundsException {
+		btns[index] = value;
+	}
+	
+	public void resetPlayArea() {
+		for(int i = 0; i < btns.length; i++) {
+			btns[i].setText("");
+			btns[i].setBackground(defaultColor);
+		}
+	}
 	
 	
 }
