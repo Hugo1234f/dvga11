7249e86543b172513c1e80f02ca8b54bf2a70ca8
Huto Friberg <hugofrib100@student.kau.se>
2022-04-20 12:29:23 +0200

    reservation more

--- Restaurangbokning/src/Reservation.java
+++ Restaurangbokning/src/Reservation.java
@@ -1,4 +1,28 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class Reservation extends QueueItem {
-
+	
+	private String name;
+	private LocalTime time;
+	private LocalDate date;
+	
+	public Reservation(String name, LocalTime time, LocalDate date) {
+		this.name = name;
+		this.time = time;
+		this.date = date;
+	}
+	
+	public String getReservationName() {
+		return name;
+	}
+	
+	public LocalTime getReservationTime() {
+		return time;
+	}
+	
+	public LocalDate getReservationDate() {
+		return date;
+	}
+	
 }
