f4f7c9cdb237ff9c807e8e33ed82c12508445d42
Huto Friberg <hugofrib100@student.kau.se>
2022-05-09 10:35:16 +0200

    initial commit labb 3

---
+++ Ordersystem/labb3.js
@@ -0,0 +1,56 @@
+
+menu = {
+	'Pizzor klass 1': [
+		{"name": "Margherita & Tomatsås", "contents": ["Ost"], "price": 65 }
+		{"name": "Vesuvio & Tomatsås", "contents": ["Ost", "Skinka"], "price": 65 }
+		{"name": "Altono & Tomatsås", "contents": ["Ost", "Tonfisk"], "price": 65 }
+	]
+	'Pizzor klass 2': [
+		{"name": "Calzone & Tomatsås", "contents": ["Ost", "Skinka"], "price": 80 }
+		{"name": "Capricciosa & Tomatsås", "contents": ["Ost", "Skinka", "Champinjoner" ], "price": 70 }
+		{"name": "Tomaso & Tomatsås", "contents": ["Ost", "Skinka", "a:Räkor" ], "price": 70 }
+		{"name": "Hawaii & Tomatsås", "contents": ["Ost", "Skinka", "Ananas" ], "price": 70 }
+		{"name": "Oriental & Tomatsås", "contents": ["Ost", "Skinka", "Köttfärs" ], "price": 70 }
+		{"name": "Venezia & Tomatsås", "contents": ["Ost", "Skinka", "Tonfisk" ], "price": 70 }
+		{"name": "Bolognese & Tomatsås", "contents": ["Ost", "Köttfärs", "Lök" ], "price": 70 }
+		{"name": "Napoli & Tomatsås", "contents": ["Ost", "Räkor", "Champinjoner" ], "price": 70 }
+	]
+	'Pizzor klass 3': [
+		{"name": "Bravo & Tomatsås", "contents": ["Ost", "Skinka", "Bacon", "Lök", "a:Ägg" ], "price": 75 }
+		{"name": "Princessa & Tomatsås", "contents": ["Ost", "Skinka", "a:Räkor}," "Champinjoner" ], "price": 75 }
+		{"name": "Kroppkärr & Tomatsås", "contents": ["Ost", "Skinka", "Köttfärs", "Champinjoner" ], "price": 75 }
+		{"name": "Afrikano & Tomatsås", "contents": ["Ost", "Skinka", "Ananas", "Curry", "Banan" ], "price": 75 }
+		{"name": "Önska & Tomatsås", "contents": ["Ost", "Skinka", "a:Räkor}," "Champinjoner" ], "price": 85 }
+		{"name": "Lambada & Tomatsås", "contents": ["Ost", "Skinka", "Köttfärs", "a:Räkor" ], "price": 75 }
+		{"name": "Alsterdalen & Tomatsås", "contents": ["Ost", "Skinka", "a:Crabfish}," "a:Räkor" ], "price": 75 }
+		{"name": "Paradis & Tomatsås", "contents": ["Ost", "Skinka", "a:Räkor}," "Ananas" ], "price": 75 }
+		{"name": "Roma & Tomatsås", "contents": ["Ost", "Skinka", "Kantareller", "Tomater (färska)" ], "price": 75 }
+		{"name": "Banjogatan & Tomatsås", "contents": ["Ost", "Skinka", "Salami", "Paprika" ], "price": 75 }
+		{"name": "Rimini & Tomatsås", "contents": ["Ost", "Köttfärs", "Gorgonzolaost", "Lök" ], "price": 75 }
+		{"name": "Opera & Tomatsås", "contents": ["Ost", "Köttfärs", "Ananas", "Curry", "Banan" ], "price": 75 }
+		{"name": "Mesopotamia & Tomatsås", "contents": ["Ost", "Salami", "Köttfärs", "a:Nötter" ], "price": 75 }
+	]
+	'Såser': [
+		{"name": "Bearnaisesås 10 cl ", "price:":  10 }
+		{"name": "Kebabsås mild 10 cl ", "price:":  10 }
+		{"name": "Kebabsås stark 10 cl ", "price:":  10 }
+		{"name": "Kebabsås blandad 10 cl ", "price:":  10 }
+		{"name": "Tzatzikisås 10 cl ", "price:":  10 }
+		{"name": "Vitlökssås 10 cl ", "price:":  10"
+	]
+	'Dryck': [
+		{"name": "Coca-Cola 33 cl ", "price:":  15 }
+		{"name": "Coca-Cola light 33 cl ", "price:":  15 }
+		{"name": "Fanta 33 cl ", "price:":  15  }
+		{"name": "Sprite 33 cl ", "price:":  15 }
+		{"name": "Mineralvatten 33 cl ", "price:":  15 }
+		{"name": "Lättöl 33 cl ", "price:":  15 }
+		{"name": "Coca-Cola 50 cl ", "price:":  20 }
+		{"name": "Fanta 50 cl ", "price:":  20 }
+	]
+}
+
+
+
+
+
